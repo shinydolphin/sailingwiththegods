f241251228513d9b8ed143674220ab46d7420ea1
Kevin Dressel <kddressel@gmail.com>
2021-09-08 18:47:28 -0400

    first stab at travis

---
+++ travis-build/build.sh
@@ -0,0 +1,92 @@
+#! /bin/sh
+
+# MIT License
+# 
+# Copyright (c) 2018 kevinta893
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+PROJECT_PATH=$(pwd)/$UNITY_PROJECT_PATH
+UNITY_BUILD_DIR=$(pwd)/build
+
+project="SailingWithTheGods"
+
+ERROR_CODE=0
+echo "Items in project path ($PROJECT_PATH):"
+ls "$PROJECT_PATH"
+
+mkdir $UNITY_BUILD_DIR
+
+############################
+
+echo "Building project for Windows..."
+/Applications/Unity/Unity.app/Contents/MacOS/Unity
+  -batchmode
+  -nographics
+  -silent-crashes
+  -logFile $(pwd)/unity.log
+  -projectPath "$PROJECT_PATH"
+  -buildWindows64Player  "$(pwd)/build/win/$project.exe"
+  -quit
+  
+if [ $? = 0 ] ; then
+  echo "Building Windows exe completed successfully."
+  ERROR_CODE=0
+else
+  echo "Building Windows exe failed. Exited with $?."
+  ERROR_CODE=1
+fi
+
+
+###########################
+
+echo "Building project for OSX..."
+mkdir $UNITY_BUILD_DIR
+/Applications/Unity/Unity.app/Contents/MacOS/Unity
+  -batchmode
+  -nographics
+  -silent-crashes
+  -logFile $(pwd)/unity.log
+  -projectPath $(pwd)
+  -buildOSXUniversalPlayer "$(pwd)/build/osx/$project.app" 
+  -quit
+
+if [ $? = 0 ] ; then
+  echo "Building OSX app completed successfully."
+  ERROR_CODE=0
+else
+  echo "Building OSX app failed. Exited with $?."
+  ERROR_CODE=1
+fi
+
+###########################
+
+
+echo 'Logs from build'
+cat $(pwd)/unity.log
+
+
+echo 'Attempting to zip builds'
+zip -r $(pwd)/Build/mac.zip $(pwd)/build/osx/
+zip -r $(pwd)/Build/windows.zip $(pwd)/build/windows/
+
+
+
+echo "Finishing with code $ERROR_CODE"
+exit $ERROR_CODE
---
+++ travis-build/install-unity.sh
@@ -0,0 +1,66 @@
+#! /bin/sh
+
+# MIT License
+# 
+# Copyright (c) 2018 kevinta893
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# See https://unity3d.com/get-unity/download/archive
+# to get download URLs
+UNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"
+UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/50fe8a171dd9/MacEditorInstaller/Unity-2019.4.9f1.pkg"
+UNITY_WINDOWS_TARGET_PACKAGE_URL="https://beta.unity3d.com/download/50fe8a171dd9/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2019.4.9f1.pkg"
+UNITY_OSX_TARGET_PACKAGE_URL="https://beta.unity3d.com/download/50fe8a171dd9/MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-2019.4.9f1.pkg"
+
+
+# Downloads a file if it does not exist
+download() {
+
+	URL=$1
+	FILE=`basename "$URL"`
+	
+	# Downloads a package if it does not already exist in cache
+	if [ ! -e $UNITY_DOWNLOAD_CACHE/`basename "$URL"` ] ; then
+		echo "$FILE does not exist. Downloading from $URL: "
+		mkdir -p "$UNITY_DOWNLOAD_CACHE"
+		curl -o $UNITY_DOWNLOAD_CACHE/`basename "$URL"` "$URL"
+	else
+		echo "$FILE Exists. Skipping download."
+	fi
+}
+
+# Downloads and installs a package from an internet URL
+install() {
+	PACKAGE_URL=$1
+	download $1
+
+	echo "Installing `basename "$PACKAGE_URL"`"
+	sudo installer -dumplog -package $UNITY_DOWNLOAD_CACHE/`basename "$PACKAGE_URL"` -target /
+}
+
+
+
+echo "Contents of Unity Download Cache:"
+ls $UNITY_DOWNLOAD_CACHE
+
+echo "Installing Unity..."
+install $UNITY_OSX_PACKAGE_URL
+install $UNITY_WINDOWS_TARGET_PACKAGE_URL
+install $UNITY_OSX_TARGET_PACKAGE_URL
