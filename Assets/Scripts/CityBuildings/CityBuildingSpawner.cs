e905114f42533118f8dce5ed0559ec56c5697e5c
Kevin Dressel <kddressel@gmail.com>
2022-03-05 10:53:26 -0500

    try a few dock positions to try and make it not overlap buildings

--- Assets/Scripts/CityBuildings/CityBuildingSpawner.cs
+++ Assets/Scripts/CityBuildings/CityBuildingSpawner.cs
@@ -15,12 +15,11 @@ public class CityBuildingSpawner : MonoBehaviour
 	[SerializeField] GameObject DockPrefab = null;
 
 	// fallback to slower FindObjectByType for editor usage
-	World World => Globals.World 
+	World World => Globals.World
 		?? GameObject.FindObjectOfType<World>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
+	// Start is called before the first frame update
+	void Start() {
 		Regenerate();
 		PlaceDock();
 	}
@@ -29,10 +28,10 @@ public class CityBuildingSpawner : MonoBehaviour
 	void Regenerate() {
 		Clear();
 
-		for(var i = 0; i < NumHouses; i++) {
+		for (var i = 0; i < NumHouses; i++) {
 			GameObject.Instantiate(
-				HousePrefabs.RandomElement(), 
-				GetBestRandomPosition(), 
+				HousePrefabs.RandomElement(),
+				GetBestRandomPosition(),
 				Quaternion.Euler(0, Random.Range(0, 360), 0),
 				transform
 			);
@@ -41,22 +40,41 @@ public class CityBuildingSpawner : MonoBehaviour
 		// kill buildings that are in the water. we don't try to replace them since it'd probably make things too dense
 		foreach (var building in transform.GetChildren().ToList()) {
 			if (World.IsBelowWaterLevel(building.position)) {
-				Debug.LogWarning("Spawned on water");
 				SafeDestroy(building.gameObject);
 			}
 		}
 	}
 
+	(Vector3 pos, Vector3 dir) GetDockPos(Vector3 cityPos) {
+		var pos = World.GetNearestPosInWater(cityPos);
+		var diffToWater = pos - cityPos;
+		var dirToWater = diffToWater.normalized;
+		var dir = Vector3.ProjectOnPlane(-dirToWater, Vector3.up);
+		var adjustedPos = pos + dir * -DockOffset;
+
+		return (adjustedPos, dir);
+	}
+
 	[Button]
 	void PlaceDock() {
-		var pos = World.GetNearestPosInWater(transform.position);
-		var diffToWater = pos - transform.position;
-		var dirToWater = diffToWater.normalized;
-		var adjustedPos = pos + dirToWater * DockOffset;
+		const int maxTries = 100;
+		var buildingMask = LayerMask.GetMask("cityBuilding");
+		var (basePos, dir) = GetDockPos(transform.position);
+
+		Vector3 bestPos = basePos;
+		for (var i = 0; i < maxTries; i++) {
+			var randomPos = Utils.RandomPositionIn(new Bounds(basePos, new Vector3(1, 0, 1)));
+
+			if (!Physics.OverlapSphere(randomPos, BuildingRadius, buildingMask).Any()) {
+				bestPos = randomPos;
+				break;
+			}
+		}
+
 		GameObject.Instantiate(
 			DockPrefab,
-			adjustedPos,
-			Quaternion.LookRotation(-dirToWater, Vector3.up),
+			bestPos,
+			Quaternion.LookRotation(dir, Vector3.up),
 			transform
 		);
 	}
@@ -71,8 +89,8 @@ public class CityBuildingSpawner : MonoBehaviour
 		var buildingMask = LayerMask.GetMask("cityBuilding");
 
 		Vector3 pos = GetRandomPositionOption();
-		for(var i = 0; i < maxTries; i++) {
-			if(!Physics.OverlapSphere(pos, BuildingRadius, buildingMask).Any()) {
+		for (var i = 0; i < maxTries; i++) {
+			if (!Physics.OverlapSphere(pos, BuildingRadius, buildingMask).Any()) {
 				return pos;
 			}
 
@@ -85,7 +103,7 @@ public class CityBuildingSpawner : MonoBehaviour
 
 	[Button]
 	void Clear() {
-		foreach(var child in transform.GetChildren().ToList()) {
+		foreach (var child in transform.GetChildren().ToList()) {
 			SafeDestroy(child.gameObject);
 		}
 	}
